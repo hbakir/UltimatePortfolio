1132b09f59eff63671d2cd9009f7ff3591687408
Hicham BAKIR <hicham@bakir.ch>
2023-08-02 11:35:25 +0200

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Hicham Bakir on 02.08.23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
