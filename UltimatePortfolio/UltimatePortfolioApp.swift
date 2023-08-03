347f6d2158f4e78347b1bfe70c5271b6833d0562
Hicham BAKIR <hicham@bakir.ch>
2023-08-03 09:24:24 +0200

    Add a data model based on CoreData

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    @StateObject var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+            
         }
     }
 }
